7e521d9278af91598ed370a1a613816482147672
unknown <wynrius12345678@gmail.com>
2024-09-20 11:58:51 +0700

    update ui

--- src/app/(main)/credit-card/(form)/layout.tsx
+++ src/app/(main)/credit-card/(form)/layout.tsx
@@ -3,15 +3,17 @@ import { FC, PropsWithChildren } from 'react'
 
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<div className="relative flex h-full w-full flex-col items-center pt-8">
-			<Image
-				src="/ruby-logo.png"
-				alt="logo"
-				width={3336}
-				height={1144}
-				className="mb-12 w-40"
-			/>
-			{children}
+		<div className="flex h-full w-full justify-center py-6">
+			<div className="flex h-fit w-fit flex-col items-center rounded-2xl border-foreground/20 p-6 lg:border lg:shadow-xl">
+				<Image
+					src="/ruby-logo.png"
+					alt="logo"
+					width={3336}
+					height={1144}
+					className="mb-12 w-40"
+				/>
+				{children}
+			</div>
 		</div>
 	)
 }
